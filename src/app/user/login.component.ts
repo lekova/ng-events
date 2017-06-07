ba1a04b31b05a563521b085e97c81a279459d138
Lina Lekova <lina.lekova@gmail.com>
2017-06-07 11:09:42 -0400

    navigate after login action

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   login(formValues) {
-    console.log('formValues', formValues);
     this.authService.loginUser(formValues.userName, formValues.password);
+    this.router.navigate(['events']);
+  }
+  cancel() {
+    this.router.navigate(['events']);
   }
 }
