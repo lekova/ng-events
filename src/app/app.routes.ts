70343a61d7a6905135a5fb4aa40e625117063100
Lina Lekova <lina.lekova@gmail.com>
2017-06-07 10:13:07 -0400

    user module with routes and profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,5 +11,6 @@ export const appRoutes: Routes = [
   { path: 'events', component: EventListComponent, resolve: {events: EventListResolver } },
   { path: 'events/:id', component: EventDetailComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
-  { path: '', redirectTo: '/events', pathMatch: 'full' }
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: 'app/user/user.module#UserModule'}
 ];
--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/RX';
+import { Subject, Observable } from 'rxjs/RX';
+import { IEvent } from './event.model';
 
-const EVENTS = [
+const EVENTS: IEvent[] = [
   {
     id: 1,
     name: 'Angular Connect',
-    date: '9/26/2036',
+    date: new Date('9/26/2036'),
     time: '10:00 am',
     price: 599.99,
     imageUrl: '/assets/images/angularconnect-shield.png',
@@ -83,12 +84,12 @@ const EVENTS = [
   {
     id: 2,
     name: 'ng-nl',
-    date: '4/15/2037',
+    date: new Date('4/15/2037'),
     time: '9:00 am',
     price: 950.00,
     imageUrl: '/assets/images/ng-nl.png',
     onlineURL: 'http://www.NG-NLConvention.com',
-    locations: {
+    location: {
       address: 'The NG-NL Convention Center & Scuba Shop',
       city: 'Amsterdam',
       country: 'Netherlands'
@@ -144,7 +145,7 @@ const EVENTS = [
   {
     id: 3,
     name: 'ng-conf 2037',
-    date: '5/4/2037',
+    date: new Date('5/4/2037'),
     time: '9:00 am',
     price: 759.00,
     imageUrl: '/assets/images/ng-conf.png',
@@ -226,7 +227,7 @@ const EVENTS = [
   {
     id: 4,
     name: 'UN Angular Summit',
-    date: '6/10/2037',
+    date: new Date('6/10/2037'),
     time: '8:00 am',
     price: 800.00,
     imageUrl: '/assets/images/basic-shield.png',
@@ -275,7 +276,7 @@ const EVENTS = [
   {
     id: 5,
     name: 'ng-vegas',
-    date: '2/10/2037',
+    date: new Date('2/10/2037'),
     time: '9:00 am',
     price: 400.00,
     imageUrl: '/assets/images/ng-vegas.png',
@@ -316,8 +317,8 @@ const EVENTS = [
 
 @Injectable()
 export class EventService {
-  getEvents() {
-    const subject = new Subject();
+  getEvents(): Observable<IEvent[]> {
+    const subject = new Subject<IEvent[]>();
     setTimeout(() => {
       subject.next(EVENTS);
       subject.complete();
@@ -325,7 +326,7 @@ export class EventService {
     return subject;
   }
 
-  getEvent(id: number) {
+  getEvent(id: number): IEvent {
     return EVENTS.find(el => el.id === id);
   }
 }
