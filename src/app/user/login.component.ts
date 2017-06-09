10067966b1869b81f4cf45445ce58aa71ccadcaf
Lina Lekova <lina.lekova@gmail.com>
2017-06-09 10:30:56 -0400

    changes to the template form in login component

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
     styles: [`
     em { float: right; color: #E05C65; padding-left: 10px; }
-    .error input {background-color: #E3C3C5; }
+    .error input { background-color: #E3C3C5; }
     .error ::-webkit-input-placeholder { color: #999; }
     .error ::-moz-placeholder { color: #999; }
     .error :-moz-placeholder { color: #999; }
@@ -18,9 +18,11 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) { }
 
   login(formValues) {
+    console.log('login called');
     this.authService.loginUser(formValues.userName, formValues.password);
     this.router.navigate(['events']);
   }
+
   cancel() {
     this.router.navigate(['events']);
   }
