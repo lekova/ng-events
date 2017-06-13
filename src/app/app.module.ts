c92d778387b81c558fba4e0312bd219e3d1c5a26
lekova <lina.lekova@gmail.com>
2017-06-12 23:07:56 -0400

    Content projection for the collapsable well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,21 @@ import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventThumbnailComponent } from './event-list/event-thumbnail.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { CreateSessionComponent } from './create-session/create-session.component';
+import { SessionListComponent } from './session-list/session-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './errors/404.component';
-import { CreateSessionComponent } from './create-session/create-session.component';
 
 import { EventService } from './shared/event.service';
 import { ToastrService } from './shared/toastr.service';
 import { AuthService } from './user/auth.service';
+import { DurationPipe } from './shared/duration.pipe';
 
 import { appRoutes } from './app.routes';
-import { CreateEventComponent } from './create-event/create-event.component';
 import { EventRouteActivator } from './shared/event-route-activator.service';
 import { EventListResolver } from './shared/event-list-resolver.service';
-import { SessionListComponent } from './session-list/session-list.component';
+import { CollapsableWellComponent } from './shared/collapsable-well.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +35,8 @@ import { SessionListComponent } from './session-list/session-list.component';
     Error404Component,
     CreateSessionComponent,
     SessionListComponent,
+    CollapsableWellComponent,
+    DurationPipe
   ],
   imports: [
     BrowserModule,
