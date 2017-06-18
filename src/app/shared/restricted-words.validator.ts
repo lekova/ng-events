c65051741207a6e848f3a45d95d9577bb343e4e9
lekova <lina.lekova@gmail.com>
2017-06-18 13:54:09 -0400

    fix css styling in profile component

--- src/app/shared/restricted-words.validator.ts
+++ src/app/shared/restricted-words.validator.ts
@@ -1,4 +1,4 @@
-import {FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 export function restrictedWords(words) {
   return (control: FormControl): {[key: string]: any} => {
